1bc610babb5a0a5e2144e28f7a1993ced00fd3d1
Cu Cong Can <cancc@vitalilfy.asia>
2022-02-18 16:38:06 +0700

    Migrate to AndroidX and change targetSdkVersion to 31

--- app/src/main/java/mbaas/com/nifcloud/androidncmbgame/MainActivity.kt
+++ app/src/main/java/mbaas/com/nifcloud/androidncmbgame/MainActivity.kt
@@ -4,11 +4,11 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.nifcloud.mbaas.core.NCMB
 import java.util.*
 
--- app/src/main/java/mbaas/com/nifcloud/androidncmbgame/RankingActivity.kt
+++ app/src/main/java/mbaas/com/nifcloud/androidncmbgame/RankingActivity.kt
@@ -2,8 +2,8 @@ package mbaas.com.nifcloud.androidncmbgame
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
